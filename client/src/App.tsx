5dd0f7ca81cb561558d215f90a1f27e7375f5850
Vladimir Solovyov <leilyngale@icloud.com>
2024-01-05 16:50:49 +0800

    Add Footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
+import Footer from './layouts/footer/Footer';
 import Carousel from './layouts/home-page/carousel/Carousel';
 import ExploreTopBooks from './layouts/home-page/ExploreTopBooks';
 import Heros from './layouts/home-page/heros/Heros';
+import LibraryServices from './layouts/home-page/library-services/LibraryServices';
 import Navbar from './layouts/navbar/Navbar';
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <ExploreTopBooks />
       <Carousel />
       <Heros />
+      <LibraryServices />
+      <Footer />
     </>
   );
 };
