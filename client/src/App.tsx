fc92962e5c481253cb94f5a0294e2d21507a8c9a
Vladimir Solovyov <leilyngale@icloud.com>
2024-01-05 16:17:47 +0800

    Add Heros component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Carousel from './layouts/home-page/carousel/Carousel'
-import ExploreTopBooks from './layouts/home-page/ExploreTopBooks'
-import Navbar from './layouts/navbar/Navbar'
+import './App.css';
+import Carousel from './layouts/home-page/carousel/Carousel';
+import ExploreTopBooks from './layouts/home-page/ExploreTopBooks';
+import Heros from './layouts/home-page/heros/Heros';
+import Navbar from './layouts/navbar/Navbar';
 
 const App = () => {
-
   return (
-   <>
-     <Navbar />
-     <ExploreTopBooks/>
-     <Carousel/>
-   </>
-  )
-}
+    <>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+    </>
+  );
+};
 
-export default App
+export default App;
